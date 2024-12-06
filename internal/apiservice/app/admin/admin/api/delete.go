945e672fe34dc2b83939fb603257fce95cd16477
270768983@qq.com <270768983@qq.com>
2024-12-06 14:52:03 +0800

    feat: admin delete 删除自己
    1. 删除自己，进制删除自己为实现，登录实现后在实现

--- internal/apiservice/app/admin/admin/api/delete.go
+++ internal/apiservice/app/admin/admin/api/delete.go
@@ -1,1 +1,74 @@
 package api
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"go-service/internal/apiservice/app/admin/admin/adminmodel"
+	"go-service/internal/apiservice/pkg/ginx"
+	"go-service/internal/apiservice/router"
+	"go-service/internal/bootstrap/appctx"
+	"gorm.io/gorm"
+)
+
+type AdminDel struct {
+	*ginx.Ginx
+	ctx    *gin.Context
+	appctx *appctx.AppCtx
+}
+
+func NewAdminDel(appctx *appctx.AppCtx, c *gin.Context) router.Handler {
+	return &AdminDel{ginx.New(c), c, appctx}
+}
+
+// Do
+// @api | admin | 5 | 删除用户
+// @path | /api/admin/:id
+// @method | DELETE
+// @header 	|n Authorization |d token |e tokenstring |c 鉴权 |t string
+// @urlparam |n id |d 用户ID |v required |t int    |e 1
+// @tbtitle  | 200 Response
+// @tbrow    |n data |e ok |c 成功 |t type
+// @response | hd.ErrResponse | 500 RESPONSE
+// @tbtitle  | msg 数据
+// @tbrow    |n msg |e 禁止删除自己
+// @tbrow    |n msg |e 记录不存在
+func (this *AdminDel) Do() error {
+	var err error
+	id, err := this.GetId()
+	if err != nil {
+		return err
+	}
+
+	//uid, err := this.appctx.GetUid(this.ctx)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//if id == uid {
+	//	return errors.New("禁止删除自己")
+	//}
+
+	if err := this.Delete(id); err != nil {
+		return err
+	} else {
+		this.RepOk()
+		return nil
+	}
+}
+
+func (this *AdminDel) Delete(id int64) error {
+	db := this.appctx.Db
+	po := &adminmodel.AdminPo{}
+	po.ID = id
+	if r := db.Take(po); r.Error != nil {
+		if r.Error == gorm.ErrRecordNotFound {
+			return errors.New("记录不存在")
+		} else {
+			return r.Error
+		}
+	}
+	if r := db.Delete(po); r.Error != nil {
+		return r.Error
+	}
+	return nil
+}
--- internal/apiservice/app/admin/admin/router.go
+++ internal/apiservice/app/admin/admin/router.go
@@ -8,12 +8,10 @@ import (
 
 // @group | admin | 1 | 用户管理 | 系统用户管理 增删查改
 func Route(appctx *appctx.AppCtx, r *router.Router) {
-	//r.Auth.GET("/admin", router.Do(appctx, api.NewAdminQuery))
-
 	r.Auth.POST("/admin", router.Do(appctx, api.NewAdminCreate))
 	r.Auth.GET("/admin", router.Do(appctx, api.NewAdminQuery))
 	r.Auth.PUT("/admin/:id", router.Do(appctx, api.NewAdminUpdate))
-	//r.Auth.DELETE("/admin/:id", router.Do(appctx, api.NewAdminDel))
+	r.Auth.DELETE("/admin/:id", router.Do(appctx, api.NewAdminDel))
 	//
 	//r.Auth.PUT("/admin/resetpwd/:id", router.Do(appctx, api.NewResetPassword))
 }
