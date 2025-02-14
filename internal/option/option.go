aa5507de47cebe3f2bc7618274d0bf1f2af24caa
270768983@qq.com <270768983@qq.com>
2025-02-14 23:46:25 +0800

    refact: 优化option的交互内容

--- internal/option/option.go
+++ internal/option/option.go
@@ -2,8 +2,8 @@ package option
 
 var Opt struct {
 	ConfigPath    string        `long:"config" short:"f" description:"配置文件路径"`
-	Version       Version       `command:"version" long:"version" short:"v" description:"版本信息"`
-	RunService    RunService    `command:"run" namespace:"group2" env-namespace:"group2" description:"api启动host"`
+	Version       Version       `command:"version" description:"版本信息"`
+	RunService    RunService    `command:"run" description:"api启动host"`
 	InitTable     InitTable     `command:"inittable"  description:"初始化数据库"`
 	InitSuperUser InitSuperUser `command:"inituser" description:"初始化超级管理员、重置超级管理员密码"`
 }
